fbfb37701d1e5d7da85d944b6db6b3a2ddab9169
idalz <idalez@outlook.com>
2024-03-25 12:08:55 +0200

    Create data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from mobilePriceRangePrediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mobilePriceRangePrediction.logging import logger
 
-logger.info("Welcome!")
+try:
+    logger.info("Data ingestion stage started.")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info("Data ingestion stage completed.")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/mobilePriceRangePrediction/components/data_ingestion.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+from mobilePriceRangePrediction.entity import DataIngestionConfig
+from mobilePriceRangePrediction.constants import *
+from mobilePriceRangePrediction.logging import logger
+from mobilePriceRangePrediction.utils.common import get_size
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_data(self):
+        if not os.path.exists(self.config.local_data_file):
+            # Initialize Kaggle API client
+            api = KaggleApi()
+
+            # Authenticate with your Kaggle API credentials
+            api.authenticate()  # This will use your Kaggle API token stored in ~/.kaggle/kaggle.json
+
+            # Download the dataset
+            dataset_name = "iabhishekofficial/mobile-price-classification"
+            api.dataset_download_files(dataset_name, path=self.config.local_data_file, unzip=True)  # Unzip the downloaded files
+
+            logger.info("Dataset downloaded successfully.")
+            print("Dataset downloaded successfully.")
+        else:
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
+            
