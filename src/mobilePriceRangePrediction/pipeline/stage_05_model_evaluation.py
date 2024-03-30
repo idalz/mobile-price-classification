c59d0632e3e79e701b1217c0d8cc2819402dcd08
idalz <idalez@outlook.com>
2024-03-30 20:38:25 +0200

    variable name changed

--- src/mobilePriceRangePrediction/pipeline/stage_05_model_evaluation.py
+++ src/mobilePriceRangePrediction/pipeline/stage_05_model_evaluation.py
@@ -10,7 +10,7 @@ class ModelEvaluationTrainingPipeline:
         try:
             config = ConfigurationManager()
             model_evaluation_config = config.get_model_evaluation_config()
-            model_trainer_config = ModelEvaluation(config=model_evaluation_config)
-            model_trainer_config.evaluate_model()
+            model_evaluation = ModelEvaluation(config=model_evaluation_config)
+            model_evaluation.evaluate_model()
         except Exception as e:
             raise e
