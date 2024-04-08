52fa654e84772dacd5ce5413929b59b42990c59b
idalz <idalez@outlook.com>
2024-04-08 19:38:35 +0300

    finished about.html

--- src/mobilePriceRangePrediction/constants/html_content.py
+++ src/mobilePriceRangePrediction/constants/html_content.py
@@ -6,8 +6,8 @@ index_dict = {
 }
 
 form_dict = {
-    "title": "Car Information Form",
-    "form_title": "Car Information Form",
+    "title": "Mobile Information Form",
+    "form_title": "Mobile Information Form",
     "form_button": "Predict Price"
 }
 
@@ -16,45 +16,49 @@ about_dict = {
     ### About the project
     "project_title": "About the Project",
     "project_descr": """
-    In the automotive market, determining the fair price of a car can be challenging for both buyers and sellers.
-    Prices can vary significantly based on factors such as make, model, year, mileage, and additional features. 
-    With such complexity, buyers often struggle to assess whether they are getting a good deal, while sellers may have difficulty setting competitive prices. 
-    This lack of transparency can lead to frustration and uncertainty in the car-buying process. <br> <br>  
+    With the ever-growing variety of mobile devices available in the market, accurately determining their class based on specifications is a challenging task.
+    Many factors such as battery power, RAM capacity, camera resolution, and connectivity options contribute to the classification process.
+    Our mission is to address this complexity and develop a solution that reliably categorizes mobile devices into their appropriate classes.<br><br>
 
-    Our <strong>main goal</strong> is to harness the power of PyTorch neural networks to develop a regression model capable of accurately <strong>predicting car prices</strong>. 
-    By leveraging machine learning techniques and a comprehensive dataset, our objective is to create a tool that empowers both buyers and sellers with reliable price estimates. 
+    <strong>Our primary goal</strong> is to leverage machine learning techniques to <strong>build a highly accurate classification model</strong> for mobile devices.
+    By analyzing extensive datasets containing mobile specifications and their corresponding classes, we aim to develop a model capable of precisely identifying
+      the class of a mobile device based on its features.
     """,
     ### About the model
     "model_title": "About the Model",
     "model_descr": """
-    Our model utilizes a simple <strong> neural network architecture implemented using PyTorch</strong>, trained on a comprehensive dataset from Kaggle.
-    With input, hidden, and output layers, the model employs fully connected linear units and activation functions to capture intricate data patterns.
-    Through iterative training using techniques like gradient descent, it minimizes prediction errors. During training, features are preprocessed, scaled, and encoded for optimal performance.
-    Leveraging PyTorch's efficiency, the model iteratively learns from the data. 
-    With a <strong>Mean Root Squared Error (MRSE) of 8145</strong>, our model showcases a good accuracy in predicting car prices, 
-    underscoring its effectiveness in capturing the nuanced relationships between car features and prices.
+    The model is a <strong>simple Linear Regression model</strong> trained on a comprehensive dataset from Kaggle.
+     During training, the features were preprocessed and scaled for optimal performance. 
+     It is able to calculate the class of a mobile based on its features 
+     with a high <strong>micro F1-score of 0.98</strong>.
+
     """,
     ### About the data
     "data_title": "About the Data",
     "data_descr": """
-    <p>The dataset used in this project was sourced from Kaggle's <a href="https://www.kaggle.com/datasets/deepcontractor/car-price-prediction-challenge" style="color: var(--primary-color-1)";>
-    Car Price Prediction Challenge</a>. 
-    It comprises a collection of car listings along with various features used for predicting car prices.</p>
+    The dataset obtained from Kaggle's <a href="https://www.kaggle.com/datasets/iabhishekofficial/mobile-price-classification" style="color: var(--primary-color-1)";>
+    Mobile Price Classification</a> consists of mobile specifications and their corresponding price classifications.<br>
 
-    <p><strong>Price:</strong> The price of the car (target variable).<br>
-    <strong>Manufacturer:</strong> The manufacturer of the car.<br>
-    <strong>Category:</strong> The category of the car.<br>
-    <strong>Leather interior:</strong> Indicates whether the car has leather interior or not.<br>
-    <strong>Fuel type:</strong> The type of fuel used by the car.<br>
-    <strong>Cylinders:</strong> The number of cylinders in the car's engine.<br>
-    <strong>Gear box type:</strong> The type of gear box used in the car.<br>
-    <strong>Drive wheels:</strong> The type of drive wheels in the car (e.g., front-wheel drive, rear-wheel drive).<br>
-    <strong>Doors:</strong> The number of doors in the car.<br>
-    <strong>Wheel:</strong> The type of wheel used in the car.<br>
-    <strong>Color:</strong> The color of the car.<br>
-    <strong>Airbags:</strong> The number of airbags in the car.<br>
-    <strong>EngineVolume:</strong> The volume of the car's engine.<br>
-    <strong>Mileage:</strong> The mileage of the car in kilometers.<br>
-    <strong>Prod. year:</strong> Year the car was produced.</p>
+    <br><strong>battery_power</strong>: The battery power of the mobile device (measured in mAh)
+    <br><strong>blue</strong>: Indicates Bluetooth support (1 for Yes, 0 for No)
+    <br><strong>clock_speed</strong>: The clock speed of the device's CPU (measured in GHz)
+    <br><strong>dual_sim</strong>: Indicates Dual SIM support (1 for Yes, 0 for No)
+    <br><strong>fc</strong>: The resolution of the front camera (in megapixels)
+    <br><strong>four_g</strong>: Indicates 4G support (1 for Yes, 0 for No)
+    <br><strong>int_memory</strong>: The internal memory capacity of the device (in GB)
+    <br><strong>m_dep</strong>: The mobile depth (measured in cm)
+    <br><strong>mobile_wt</strong>: The weight of the mobile device (measured in grams)
+    <br><strong>n_cores</strong>: The number of CPU cores
+    <br><strong>pc</strong>: The resolution of the primary camera (in megapixels)
+    <br><strong>px_height</strong>: The pixel height of the device's display
+    <br><strong>px_width</strong>: The pixel width of the device's display
+    <br><strong>ram</strong>: The RAM capacity of the device (measured in megabytes)
+    <br><strong>sc_h</strong>: The screen height of the device (measured in cm)
+    <br><strong>sc_w</strong>: The screen width of the device (measured in cm)
+    <br><strong>talk_time</strong>: The talk time of the device (measured in hours)
+    <br><strong>three_g</strong>: Indicates 3G support (1 for Yes, 0 for No)
+    <br><strong>touch_screen</strong>: Indicates Touch Screen presence (1 for Yes, 0 for No)
+    <br><strong>wifi</strong>: Indicates Wi-Fi support (1 for Yes, 0 for No)
+    <br><strong>price_range</strong>: The price range of the mobile device (categorized)
     """,
 }
